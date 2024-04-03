6d8bfe520c61b31bcd514b7768d032ec53b29634
EmanueleUgolotti <162732357+EmanueleUgolotti@users.noreply.github.com>
2024-04-03 09:26:35 +0200

    LocalOpts.assignment1.1.cpp
    Asignment1.1

--- Assignment1/LocalOpts.assignment1.1.cpp
+++ Assignment1/LocalOpts.assignment1.1.cpp
@@ -15,57 +15,6 @@
 using namespace llvm;
 
 bool runOnBasicBlock(BasicBlock &B) {
-    
-    // Preleviamo le prime due istruzioni del BB
-    Instruction &Inst1st = *B.begin(), &Inst2nd = *(++B.begin());
-
-
-    // L'indirizzo della prima istruzione deve essere uguale a quello del 
-    // primo operando della seconda istruzione (per costruzione dell'esempio)
-    assert(&Inst1st == Inst2nd.getOperand(0));
-
-    // Stampa la prima istruzione
-    outs() << "PRIMA ISTRUZIONE: " << Inst1st << "\n";
-    // Stampa la prima istruzione come operando
-    outs() << "COME OPERANDO: ";
-    Inst1st.printAsOperand(outs(), false);
-    outs() << "\n";
-
-    // User-->Use-->Value
-    outs() << "I MIEI OPERANDI SONO:\n";
-    for (auto *Iter = Inst1st.op_begin(); Iter != Inst1st.op_end(); ++Iter) {
-      Value *Operand = *Iter;
-
-      if (Argument *Arg = dyn_cast<Argument>(Operand)) {
-        outs() << "\t" << *Arg << ": SONO L'ARGOMENTO N. " << Arg->getArgNo() 
-	       <<" DELLA FUNZIONE " << Arg->getParent()->getName()
-               << "\n";
-      }
-      if (ConstantInt *C = dyn_cast<ConstantInt>(Operand)) {
-        outs() << "\t" << *C << ": SONO UNA COSTANTE INTERA DI VALORE " << C->getValue()
-               << "\n";
-      }
-    }
-
-    outs() << "LA LISTA DEI MIEI USERS:\n";
-    for (auto Iter = Inst1st.user_begin(); Iter != Inst1st.user_end(); ++Iter) {
-      outs() << "\t" << *(dyn_cast<Instruction>(*Iter)) << "\n";
-    }
-
-    outs() << "E DEI MIEI USI (CHE E' LA STESSA):\n";
-    for (auto Iter = Inst1st.use_begin(); Iter != Inst1st.use_end(); ++Iter) {
-      outs() << "\t" << *(dyn_cast<Instruction>(Iter->getUser())) << "\n";
-    }
-
-    // Manipolazione delle istruzioni
-    Instruction *NewInst = BinaryOperator::Create(
-    Instruction::Add, Inst1st.getOperand(0), Inst1st.getOperand(1));
-
-    NewInst->insertAfter(&Inst1st);
-    // Si possono aggiornare le singole references separatamente?
-    // Controlla la documentazione e prova a rispondere.
-    Inst1st.replaceAllUsesWith(NewInst);
-
 
   // All'interno della funzione runOnBasicBlock
   //FATTO DA ME
@@ -92,29 +41,45 @@ bool runOnBasicBlock(BasicBlock &B) {
           Value *Op0 = Inst.getOperand(0);
           Value *Op1 = Inst.getOperand(1);
           Constant *Zero = ConstantInt::get(Op0->getType(), 0);
-          if (Op0 == Zero) {
-              Inst.replaceAllUsesWith(Op1);
-              Inst.eraseFromParent();
-              continue;
-          } else if (Op1 == Zero) {
-              Inst.replaceAllUsesWith(Op0);
-              Inst.eraseFromParent();
-              continue;
+
+          if (auto *Op0Const = dyn_cast<ConstantInt>(Op0)) {
+              if (Op0Const->isZero()) {
+                  Inst.replaceAllUsesWith(Op1);
+                  Inst.eraseFromParent();
+                  continue;
+              }
+          } else if (auto *Op1Const = dyn_cast<ConstantInt>(Op1)) {
+              if (Op1Const->isZero()) {
+                  Inst.replaceAllUsesWith(Op0);
+                  Inst.eraseFromParent();
+                  continue;
+              }
           }
       }
 
       // Ottimizzazione per la moltiplicazione per 1
-      if (Inst.getOpcode() == Instruction::Mul) {
-          Value *Op0 = Inst.getOperand(0);
-          Value *Op1 = Inst.getOperand(1);
-          Constant *One = ConstantInt::get(Op0->getType(), 1); 
-          if (Op0 == One || Op1 == One) {
-              Inst.replaceAllUsesWith(Op0 == One ? Op1 : Op0);
-              Inst.eraseFromParent();
-              continue;
-          }
-      }
-      return true;
+if (Inst.getOpcode() == Instruction::Mul) {
+    Value *Op0 = Inst.getOperand(0);
+    Value *Op1 = Inst.getOperand(1);
+    Constant *One = ConstantInt::get(Op0->getType(), 1); 
+
+    if (auto *Op0Const = dyn_cast<ConstantInt>(Op0)) {
+        if (Op0Const->equalsInt(1)) {
+            Inst.replaceAllUsesWith(Op1);
+            Inst.eraseFromParent();
+            continue;
+        }
+    } else if (auto *Op1Const = dyn_cast<ConstantInt>(Op1)) {
+        if (Op1Const->equalsInt(1)) {
+            Inst.replaceAllUsesWith(Op0);
+            Inst.eraseFromParent();
+            continue;
+        }
+    }
+}
+
+return true;
+
   }
 }
 
