15a6228bbf43c48624b2cfbde3081fc7fbf9a011
EmanueleUgolotti <162732357+EmanueleUgolotti@users.noreply.github.com>
2024-05-19 18:08:41 +0200

    Update LICM.h finale

--- Assignment3/LICM.h
+++ Assignment3/LICM.h
@@ -2,18 +2,16 @@
  * Codice per testare le chains delle funzioni, 2024-04-22
  * */
 
-#ifndef LLVM_TRANSFORMS_CHAINS_H
-#define LLVM_TRANSFORMS_CHAINS_H
+#ifndef LLVM_TRANSFORMS_LOOPPASSES_H
+#define LLVM_TRANSFORMS_LOOPPASSES_H
 #include "llvm/IR/PassManager.h"
 #include "llvm/Transforms/Scalar/LoopPassManager.h"
 #include "llvm/Analysis/LoopAnalysisManager.h"
-namespace llvm {
-class Chains : public PassInfoMixin<Chains> {
-public:
-  PreservedAnalyses run(Loop &L, LoopAnalysisManager &LAM,
-                        LoopStandardAnalysisResults &LAR, LPMUpdater &LU);
 
-  // PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
-};
-} // namespace llvm
-#endif // LLVM_TRANSFORMS_TESTPASS _H
+namespace llvm {
+    class LoopPassEs : public PassInfoMixin<LoopPassEs>{
+        PreservedAnalyses run(Loop &L, LoopAnalysisManager &LAM, LoopStandardAnalysisResults &LAR, LPMUpdater &LU);
+
+    }; //namespace llvm
+}
+#endif // LLVM_TRANSFORMS_LOOPPASSES_H
