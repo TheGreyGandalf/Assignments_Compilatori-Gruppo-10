c5282a7b243519567457c53588e06becfa87f6b7
Gandalf <118108075+TheGreyGandalf@users.noreply.github.com>
2024-06-06 16:31:08 +0200

    Update setup2.sh

--- Assignment4/setup2.sh
+++ Assignment4/setup2.sh
@@ -1,4 +1,3 @@
-#clang -O0 -emit-llvm -S -c ciclo.cpp -o Fusion.ll
 #rimuovere la parte symbols da quello prodotto nella parte precedente
-opt -p LoopFusionPass Fusion.ll -o Fusion.bc
+opt -p LoopFusionPass Fusion-passo.ll -o Fusion.bc
 llvm-dis Fusion.bc -o Fusion-finale.ll
